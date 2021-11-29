6e8d85ba717042ef4f6e5f4a9c8c0f8f7695ddd7
Danielle Remmerswaal <d.m.remmerswaal@students.uu.nl>
2021-11-29 16:15:07 +0100

    Update 4 Posteriors.R

--- 4 Posteriors.R
+++ 4 Posteriors.R
@@ -4,7 +4,6 @@
 #P(scorepattern|Class=1)=P(Q1=A1|X=1) * P(Q2=A2|X=1)* P(Q3=A3|X=1) 
       #necessary to calculate the conditional probabilities P(Yn|Class)
 #P(scorepattern)=P(Class=1)P(Y|Class)+ ... +P(Class=C)P(Y|Class=C).
-  
 
 #datasets: 
 cbind(head(Dat_var_A), head(Dat_var_B), head(Dat_var_C), head(Dat_var_D))
@@ -12,15 +11,8 @@ props_A <- prop.table(summary(as.factor(Dat_var_A$trueclass))) #proportions of e
 props_B <- prop.table(summary(as.factor(Dat_var_B$trueclass))) #proportions of each class
 props_C <- prop.table(summary(as.factor(Dat_var_C$trueclass))) #proportions of each class
 props_D <- prop.table(summary(as.factor(Dat_var_D$trueclass))) #proportions of each class
-props_A
+#deze code staat ook in de posteriorfunction
 
-poLCA.posterior(lca,simdata) #function to calculate posterior
-#we need to calculate the posteriors by hand, because the function does not work anymore on the imputed datasets
-#PROBLEM: we manipulated the datasets (by combining and imputing) so we are not able to access $probs of an polca object anymore.
-#uitwerken hoe dit zelf kan zonder $probs -> we zien meteen hoe goed de structuur behouden blijft
-
-props_A[1] #proportion being in class 1
-dat_sectoren$probs
 dat1$probs #voorbeeld, van dataset met 20% selection error en twee klassen 
 dat_sectoren$probs #voorbeeld, van dataset met 20% selection error en drie klassen 
 #doel: nu een probs tabel met vier klassen maken... op basis van geobserveerde score patterns en true class
@@ -41,6 +33,7 @@ Conditionals_Y2 <- matrix(data=NA,nrow=4, ncol=4)
 Conditionals_Y3 <- matrix(data=NA,nrow=4, ncol=4)
 Conditionals_Y4 <- matrix(data=NA,nrow=4, ncol=4)
 
+#two for loops to create four 4x4 matrices 
 for (c in 1:4){ #rows, iterate over classes 
   for (i in 1:4){ #columns, iterate over answers
   Conditionals_Y1[c,i] <- sum(list_classes[[c]]$Y1[which(list_classes[[c]]$Y1==i)])/sum(list_classes[[c]]$Y1)
@@ -51,7 +44,7 @@ for (c in 1:4){ #rows, iterate over classes
 conditionals <- list(Conditionals_Y1, Conditionals_Y2,Conditionals_Y3 ,Conditionals_Y4 )
 conditionals    #resultaten zijn opgeslagen in een list van matrices
 
-
+#deze drie code blokken kunnen evt in de functie erbij. en misschien kan de code nog compacter?
         
 
 #-------------------------------function to calculate the posterior probabilities-----------#
@@ -62,10 +55,9 @@ Pclasses <- prop.table(summary(as.factor(dataset$trueclass))) #proportions of ea
     prob.y <- c()
     posterior_probs <- array(NA,dim=c(ssize,4))
     #function to calculate posterior probabilities, P(class|scores)
-    posterior_function <- function(dataset,  ssize=5000, conditionals){
-      
-      Pclasses <- prop.table(summary(as.factor(dataset$trueclass))) #proportions of each class
-            for(i in 1:ssize){ #nr of observations in dataset
+    posterior_function <- function(dataset,  ssize=5000, conditionals){ #need to provide a dataset and the conditional probabilities, P(score|class). Default samplesize is set to 5000
+            Pclasses <- prop.table(summary(as.factor(dataset$trueclass))) #proportions of each class
+      for(i in 1:ssize){ #nr of observations in dataset
       for(c in 1:4){ #nr of classes
         prob.y.given.x[i,c] <- conditionals[[1]][c,(dataset[i,1])]*conditionals[[2]][c,(dataset[i,2])]*conditionals[[3]][c,(dataset[i,3])]*conditionals[[4]][c,(dataset[i,4])]
       }}
@@ -76,11 +68,10 @@ Pclasses <- prop.table(summary(as.factor(dataset$trueclass))) #proportions of ea
       for(c in 1:4){ #nr of classes
         posterior_probs[i,c] <-  (Pclasses[c]*prob.y.given.x[i,c])/prob.y[i]  
       }}
-      posterior_probs<<-posterior_probs #assign result to the environment (outside the function)
+      posterior_probs<<-posterior_probs #assign result to the environment (to be able to access outside the function)
       }#end function
     posterior_function(dataset = Dat_var_A, conditionals = conditionals)
    #show first five results 
     posterior_probs[1:5,]
  #conclusion: most cases in dataset A can be quite confidently assigned to one class via modal assignment        
 
-    #what would happen with larger errors?
